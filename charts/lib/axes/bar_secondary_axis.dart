981a3fdd777cee8ed5b4cc7d559793292e8d3858
Aniket Agarwal <aniketkagarwal2312@gmail.com>
2019-12-21 14:17:39 +0530

    Create bar_secondary_axis.dart

---
+++ charts/lib/axes/bar_secondary_axis.dart
@@ -0,0 +1,109 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class BarChartWithSecondaryAxis extends StatelessWidget {
+  static const secondaryMeasureAxisId = 'secondaryMeasureAxisId';
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  BarChartWithSecondaryAxis(this.seriesList, {this.animate});
+
+  factory BarChartWithSecondaryAxis.withSampleData() {
+    return new BarChartWithSecondaryAxis(
+      _createSampleData(),
+      animate: false,
+    );
+  }
+
+  factory BarChartWithSecondaryAxis.withRandomData() {
+    return new BarChartWithSecondaryAxis(_createRandomData());
+  }
+
+  static List<charts.Series<OrdinalSales, String>> _createRandomData() {
+    final random = new Random();
+
+    final globalSalesData = [
+      new OrdinalSales('2014', random.nextInt(100) * 100),
+      new OrdinalSales('2015', random.nextInt(100) * 100),
+      new OrdinalSales('2016', random.nextInt(100) * 100),
+      new OrdinalSales('2017', random.nextInt(100) * 100),
+    ];
+
+    final losAngelesSalesData = [
+      new OrdinalSales('2014', random.nextInt(100)),
+      new OrdinalSales('2015', random.nextInt(100)),
+      new OrdinalSales('2016', random.nextInt(100)),
+      new OrdinalSales('2017', random.nextInt(100)),
+    ];
+
+    return [
+      new charts.Series<OrdinalSales, String>(
+        id: 'Global Revenue',
+        domainFn: (OrdinalSales sales, _) => sales.year,
+        measureFn: (OrdinalSales sales, _) => sales.sales,
+        data: globalSalesData,
+      ),
+      new charts.Series<OrdinalSales, String>(
+        id: 'Los Angeles Revenue',
+        domainFn: (OrdinalSales sales, _) => sales.year,
+        measureFn: (OrdinalSales sales, _) => sales.sales,
+        data: losAngelesSalesData,
+      )..setAttribute(charts.measureAxisIdKey, secondaryMeasureAxisId)
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.BarChart(
+      seriesList,
+      animate: animate,
+      barGroupingType: charts.BarGroupingType.grouped,
+      primaryMeasureAxis: new charts.NumericAxisSpec(
+          tickProviderSpec:
+              new charts.BasicNumericTickProviderSpec(desiredTickCount: 3)),
+      secondaryMeasureAxis: new charts.NumericAxisSpec(
+          tickProviderSpec:
+              new charts.BasicNumericTickProviderSpec(desiredTickCount: 3)),
+    );
+  }
+
+  static List<charts.Series<OrdinalSales, String>> _createSampleData() {
+    final globalSalesData = [
+      new OrdinalSales('2014', 5000),
+      new OrdinalSales('2015', 25000),
+      new OrdinalSales('2016', 100000),
+      new OrdinalSales('2017', 750000),
+    ];
+
+    final losAngelesSalesData = [
+      new OrdinalSales('2014', 25),
+      new OrdinalSales('2015', 50),
+      new OrdinalSales('2016', 10),
+      new OrdinalSales('2017', 20),
+    ];
+
+    return [
+      new charts.Series<OrdinalSales, String>(
+        id: 'Global Revenue',
+        domainFn: (OrdinalSales sales, _) => sales.year,
+        measureFn: (OrdinalSales sales, _) => sales.sales,
+        data: globalSalesData,
+      ),
+      new charts.Series<OrdinalSales, String>(
+        id: 'Los Angeles Revenue',
+        domainFn: (OrdinalSales sales, _) => sales.year,
+        measureFn: (OrdinalSales sales, _) => sales.sales,
+        data: losAngelesSalesData,
+      )..setAttribute(charts.measureAxisIdKey, secondaryMeasureAxisId)
+    ];
+  }
+}
+
+class OrdinalSales {
+  final String year;
+  final int sales;
+
+  OrdinalSales(this.year, this.sales);
+}
